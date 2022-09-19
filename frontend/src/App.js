7f1998d493da619db2fab0a5c341e5571397e204
adrncl <104319693+adrncl@users.noreply.github.com>
2022-09-19 19:05:35 -0400

    Update MapContainer centre and zoom level
    Update to Level 5 and north of center of North America

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 
 function App() {
   return (
-    <MapContainer center={[43.71, -79.42]} zoom={12} scrollWheelZoom={true}>
+    <MapContainer center={[49.411, -97.646]} zoom={5} scrollWheelZoom={true}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
